8c81a9588701ea0d62b2ef731c7f16ed22c813c6
Momin Khan <foolmoron@gmail.com>
2015-04-17 23:23:41 -0400

    Initial commit, can press buttons to make a blast animation

---
+++ Assets/Scripts/AnimUtils.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimUtils : MonoBehaviour {
+
+    public void DestroySelf() {
+        Destroy(gameObject);
+    }
+
+    public void DestroyParent() {
+        Destroy(transform.parent.gameObject);
+    }
+}
